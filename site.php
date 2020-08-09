818322f6b667274654316774dde20593ebcb3fcc
Tarcisio Alves Fernandes <tarcisiorevolta@gmail.com>
2020-08-09 18:37:29 -0300

    Categorias VS Produtos

--- site.php
+++ site.php
@@ -3,6 +3,7 @@
 require_once("vendor/autoload.php");
 use \Hcode\Page;
 use \Hcode\Model\Product;
+use \Hcode\Model\Category;
 
 $app->get('/', function() {
    
@@ -14,6 +15,21 @@ $app->get('/', function() {
 		"products"=>Product::checkList($products)
 	]);
 
+
+});
+
+$app->get("/categories/:idcategory", function($idcategory){
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page = new Page();
+	
+	$page->setTpl("category", [
+		"category"=>$category->getValues(),
+		"products"=>Product::checkList($category->getProducts())
+	]);
 
 });
 
